8386ff5629740581057660a8f3ddd9efe61c1bfa
Nur Kholifah <nkh.nurkholifah@gmail.com>
2026-06-04 02:34:51 +0300

    Add macro_turkey dataset

---
+++ R/macro_turkey.R
@@ -0,0 +1,21 @@
+#' Turkish Macroeconomic Data
+#'
+#' Monthly macroeconomic indicators used to illustrate
+#' semiautoReg functions.
+#'
+#' @format A data frame with 133 observations and 7 variables:
+#' \describe{
+#'   \item{Tarih}{Observation date}
+#'   \item{inflasi}{Inflation rate}
+#'   \item{kurs}{Exchange rate}
+#'   \item{m3}{Money supply}
+#'   \item{industri}{Industrial production index}
+#'   \item{kredit}{Credit volume}
+#'   \item{brent}{Brent crude oil price}
+#' }
+#'
+#' @source Public macroeconomic indicators for Türkiye.
+#'
+#' @docType data
+#' @name macro_turkey
+NULL
